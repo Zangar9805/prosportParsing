1b7bb737399a0567c927a89444189de749cd5a96
zangar9805 <zangar9805@gmail.com>
2019-06-30 18:40:44 +0600

    second commit

--- src/kz/zangpro/jsoup/Repository.kt
+++ src/kz/zangpro/jsoup/Repository.kt
@@ -1,7 +1,6 @@
 package kz.zangpro.jsoup
 
 import org.jsoup.Jsoup
-import org.jsoup.select.Elements
 
 class Repository {
 
@@ -11,7 +10,7 @@ class Repository {
     private var imgUrl = ""
     private var goals = ""
     private var runk = 0
-    private var point = 0;
+    private var point = 0
     private var wins = 0
     private var draws = 0
     private var losts = 0
@@ -27,25 +26,24 @@ class Repository {
 
         val table = doc.select("div.table.active").select("table").select("tbody")
         val trs = table.select("tr")
-        var tds: Elements? = null
         val listTable = mutableListOf<TablesModel>()
 
         for (i in trs.indices) {
-            tds = trs.get(i).select("td")
+            val tds = trs[i].select("td")
             for (j in tds.indices) {
 
                 when (j) {
-                    0 -> runk = tds.get(j).select("span").text().toInt()
+                    0 -> runk = tds[j].select("span").text().toInt()
                     1 -> {
-                        name = tds.get(j).select("div.title").select("a").text().split(" ")[0]
-                        imgUrl = tds.get(j).select("div.flag").select("img").attr("src")
+                        name = tds[j].select("div.title").select("a").text().split(" ")[0]
+                        imgUrl = tds[j].select("div.flag").select("img").attr("src")
                     }
-                    2 -> games = tds.get(j).text().toInt()
-                    3 -> wins = tds.get(j).text().toInt()
-                    4 -> draws = tds.get(j).text().toInt()
-                    5 -> losts = tds.get(j).text().toInt()
-                    6 -> goals = tds.get(j).text()
-                    7 -> point = tds.get(j).text().toInt()
+                    2 -> games = tds[j].text().toInt()
+                    3 -> wins = tds[j].text().toInt()
+                    4 -> draws = tds[j].text().toInt()
+                    5 -> losts = tds[j].text().toInt()
+                    6 -> goals = tds[j].text()
+                    7 -> point = tds[j].text().toInt()
                 }
             }
             listTable.add(TablesModel(runk, name, imgUrl, games, wins, draws, losts, goals, point))
@@ -57,7 +55,6 @@ class Repository {
     fun getKPLNews(): MutableList<NewsModel>{
         val listNews = mutableListOf<NewsModel>()
         val doc = Jsoup.connect(url2).get()
-        var sum = 0
 
         val news = doc.select("div.content").select("div.news").select("div.items").select("div.item")
 
